34eb1c0849f5d2394001b98570b7227e0fe29855
horaotoko <horaotoko@gmail.com>
2014-07-20 22:17:56 +0900

    勉強終了した

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ActiveRecord::Base
+  scope :costly, -> { where("price > ?", 3000) }
+  scope :written_about, -> (theme) { where("name like ?", "%#{theme}%") }
+
+  belongs_to :publisher
+  
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 end
